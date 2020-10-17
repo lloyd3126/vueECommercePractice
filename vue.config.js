b2a1ed99f55b8c990b494fd53c16afd6efa0bac0
Chen Chung Nien <lloyd3126@gmail.com>
2020-10-17 13:27:13 +0800

    更改 ico 和 index 的 title

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/vueECommercePractice/'
-      : ''
+      : '',
+
+      pages: {
+        index: {
+          entry: 'src/main.js',
+          title: '四分之一蛋糕工作室',
+        }
+      }
   }
 
